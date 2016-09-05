447f9dede67426d6df0fa2d19661a4e910e17218
Luke <luke@luke.co.pl>
2016-09-05 21:46:12 +0200

    Add SpritesManager.ts

---
+++ src/common/SpritesManager.ts
@@ -0,0 +1,53 @@
+interface Pos {
+    x: number;
+    y: number;
+}
+
+interface Size {
+    width: number;
+    height: number;
+}
+
+interface SpriteInfo {
+    spriteSheet: HTMLImageElement,
+    offset: Pos,
+    size: Size
+}
+
+export class SpritesManager {
+    private spriteSheets: { [name: string]: HTMLImageElement };
+    private spritesInfo: { [name: string]: SpriteInfo };
+
+    constructor(private context: CanvasRenderingContext2D) {
+        this.loadSpriteSheets();
+
+        this.addSpriteInfo('flyDead', 'enemies', { x: 143, y: 0 }, { width: 59, height: 33 });
+        this.addSpriteInfo('flyFly1', 'enemies', { x: 0, y: 32 }, { width: 72, height: 36 });
+        this.addSpriteInfo('flyFly2', 'enemies', { x: 0, y: 0 }, { width: 75, height: 31 });
+    }
+
+    drawSprite(name: string, position: Pos) {
+        let spriteInfo = this.spritesInfo[name];
+        this.context.drawImage(spriteInfo.spriteSheet,
+                               spriteInfo.offset.x, spriteInfo.offset.y,
+                               spriteInfo.size.width, spriteInfo.size.height);
+    }
+
+    private loadSpriteSheet(name: string, path: string) {
+        let image = new Image();
+        image.src = 'assets/sprites/' + path;
+        this.spriteSheets[name] = image;
+    }
+
+    private loadSpriteSheets() {
+        this.loadSpriteSheet('enemies', 'enemies_spritesheet.png');
+    }
+
+    private addSpriteInfo(name: string, spriteSheetName: string, offset: Pos, size: Size) {
+        this.spritesInfo[name] = {
+            spriteSheet: this.spriteSheets[spriteSheetName],
+            offset: offset,
+            size: size
+        };
+    }
+}
